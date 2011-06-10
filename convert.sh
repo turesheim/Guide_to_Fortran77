1fbd5b7cfc755e3c850f32c53e9f3a1706e6a33a
Torkild U. Resheim <torkildr@gmail.com>
2011-06-10 15:47:19 +0200

    Removes <HR> and fixes bad quotes and headers.
    Change-Id: Idedeee5d7476822553fa6d919a7452a63d509c7d

--- convert.sh
+++ convert.sh
@@ -4,5 +4,18 @@
 rm -rf prof77
 latex2html -split=+1 -link=0 -no_navigation -noaddress \
 	-html_version 4.0,unicode -image_type=gif prof77.tex
+
+# Attempt to clean up some more mess
 tidy -clean -utf8 -asxhtml -m prof77/*.html
-#sed "s/\`\`//g" prof77/*html
+
+# Remove <hr /> at the end of each chapter
+sed -i .backup -e 's/<hr \/>//g' prof77/*.html
+
+# Fix bad quotes
+sed -i .backup -e "s/\'\'/\&quot;/g" prof77/*.html
+sed -i .backup -e "s/\`\`/\&quot;/g" prof77/*.html
+
+# Headers
+sed -i .backup -e "s/><a name=\"SECTION.*/ id=/g" prof77/*.html
+sed -i .backup -e "s/<\/a><\/h/<\/h/g" prof77/*.html
+
