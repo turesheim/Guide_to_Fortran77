f7f641bc188e99111cfd9adf7c8a1ef5b02e6b66
Torkild U. Resheim <torkildr@gmail.com>
2011-06-10 21:54:28 +0200

    Improved formatting and compliance with ePUB.
    Change-Id: I929fdf944e8d362bf2d151f06556a191bab6342f

--- convert.sh
+++ convert.sh
@@ -2,20 +2,21 @@
 # We should be using PNG images. But there appears to be a bug in netpbm that
 # produces no images at all when in this mode.
 rm -rf prof77
-latex2html -split=+1 -link=0 -no_navigation -noaddress \
-	-html_version 4.0,unicode -image_type=gif prof77.tex
+latex2html -split=+1 -html_version 3.2,unicode -image_type=gif prof77.tex
 
 # Attempt to clean up some more mess
-tidy -clean -utf8 -asxhtml -m prof77/*.html
+tidy -config tidy.txt -m prof77/*.html
 
 # Remove <hr /> at the end of each chapter
-sed -i .backup -e 's/<hr \/>//g' prof77/*.html
+#sed -i .backup -e 's/<hr \/>//g' prof77/*.html
 
 # Fix bad quotes
-sed -i .backup -e "s/\'\'/\&quot;/g" prof77/*.html
-sed -i .backup -e "s/\`\`/\&quot;/g" prof77/*.html
+#sed -i .backup -e "s/\'\'/\&quot;/g" prof77/*.html
+#sed -i .backup -e "s/\`\`/\&quot;/g" prof77/*.html
 
-# Headers
+# Fix headers
 sed -i .backup -e "s/><a name=\"SECTION.*/ id=/g" prof77/*.html
 sed -i .backup -e "s/<\/a><\/h/<\/h/g" prof77/*.html
 
+# Various HTML
+sed -i .backup -e "s/<br clear=\"all\" \/>/<br\/>/g" prof77/*.html
